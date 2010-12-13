497c87df3e2a60b5c8c2da8b0a3246c065833f21
websitezcom <websitezcom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-13 07:12:07 +0000

    1.6.7
    git-svn-id: https://plugins.svn.wordpress.org/wp-mobile-detector/tags/1.6.7@322170 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/anakin-mobile/header.php
+++ themes/anakin-mobile/header.php
@@ -6,14 +6,14 @@
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats please -->
   <meta name="description" content="<?php bloginfo('description'); ?>" />
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" type="text/css" media="screen" />
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/jquery-mobile-min.css" />
-	<script src="<?php bloginfo('template_directory'); ?>/jquery-1.4.3.min.js"></script>
-	<script src="<?php bloginfo('template_directory'); ?>/jquery-mobile-min.js"></script>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/jquery-mobile-min.css" />
+	<script src="<?php echo get_template_directory_uri(); ?>/jquery-1.4.3.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/jquery-mobile-min.js"></script>
 	<?php wp_get_archives('type=monthly&format=link'); ?>
 	<?php //comments_popup_script(); // off by default ?>
 	<script type="text/javascript">
@@ -29,7 +29,7 @@
 	<div id="wrapper_mobile" data-role="page">
 		<div class="ui-grid-a" data-role="header" data-theme="a" style="margin-bottom: 5px;">
 			<h1><?php bloginfo('name'); ?></h1>
-			<a href="" data-role="button" data-inline="true" data-icon="plus" data-iconpos="notext" onClick="websitez_extendMenu();" class="ui-btn-right header-expand"></a>
+			<a href="" data-role="button" data-inline="true" data-icon="plus" data-iconpos="notext" onClick="websitez_extendMenu(); return false;" class="ui-btn-right header-expand"></a>
 			<div class="wrapper exMenu" style="display: none;">
 				<?php get_search_form(); ?>
 			</div>
--- websitez-wp-mobile-detector.php
+++ websitez-wp-mobile-detector.php
@@ -1,10 +1,10 @@
-<?
+<?php
 /**
  Plugin Name: WP Mobile Detector
  Plugin URI: http://www.websitez.com/
  Description: The WP Mobile Detector wordpress plugin automatically detects if the visitor is using a standard mobile phone or a smart phone and loads a compatible wordpress mobile theme for each. This plugin is one of the first to differentiate between a standard mobile phone and a smart phone. With advanced mobile statistics, image resizing, automatically formatted content, and detection of 5,000+ mobile phones, the WP Mobile Detector gives your mobile visitors the experience they desire.
 
- Version: 1.6.4
+ Version: 1.6.6
  Author: Websitez.com
  Author URI: http://www.websitez.com
 */
@@ -33,6 +33,8 @@ define('WEBSITEZ_ADVANCED_MAX_IMAGE_WIDTH', '250');
 define('WEBSITEZ_STATS_TABLE', $table_prefix.'websitez_stats');
 define('WEBSITEZ_RECORD_STATS_NAME', 'websitez_record_stats');
 define('WEBSITEZ_RECORD_STATS', "true");
+define('WEBSITEZ_SHOW_ATTRIBUTION_NAME', 'websitez_show_attribution');
+define('WEBSITEZ_SHOW_ATTRIBUTION', "false");
 define('WEBSITEZ_USE_PREINSTALLED_THEMES', "true");
 define('WEBSITEZ_USE_PREINSTALLED_THEMES_NAME', "websitez_preinstalled_themes");
 define('WEBSITEZ_BASIC_URL_REDIRECT', 'websitez_basic_url_redirect');
@@ -40,7 +42,7 @@ define('WEBSITEZ_ADVANCED_URL_REDIRECT', 'websitez_advanced_url_redirect');
 
 //Does this plugin come with pre-installed templates?
 global $websitez_preinstalled_templates;
-$websitez_preinstalled_templates = "true";
+$websitez_preinstalled_templates = get_option(WEBSITEZ_USE_PREINSTALLED_THEMES_NAME);
 
 // Install plugin
 if(function_exists('register_activation_hook')) {
@@ -49,6 +51,7 @@ if(function_exists('register_activation_hook')) {
 
 if(is_admin()) {
 	require(dirname(__FILE__) . '/admin/admin-page.php');
+	require(dirname(__FILE__) . '/admin/themes.php');
 	add_action('admin_menu', 'websitez_configuration_menu');
 	//Check to make sure plugin is installed properly
 	add_action('init', 'websitez_checkInstalled');
